f02971b926825a5841568d10a4120ecc12de7841
Nick <coffeebeforearch@gmail.com>
2019-02-25 22:34:50 -0500

    Add existence checking example

--- fundamental_concepts/modules/os_module/exists.py
+++ fundamental_concepts/modules/os_module/exists.py
@@ -1,3 +1,13 @@
 # This program uses the "os" module to verify that a file exists
 # By: Nick from CoffeeBeforeArch
 
+from sys import argv
+
+# Import exists to check file existence
+from os.path import exists
+
+# Get program name and file path from command line
+program_name, path_to_file = argv
+
+# Check to see the existence of a file
+print(exists(path_to_file))
