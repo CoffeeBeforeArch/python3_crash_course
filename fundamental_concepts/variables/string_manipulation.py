b0b40f10c428fd937c7ecf580c72e0c45f76d22a
Nick <CoffeeBeforeArch@gmail.com>
2019-03-06 08:35:47 -0500

    Add string example

---
+++ fundamental_concepts/variables/string_manipulation.py
@@ -0,0 +1,19 @@
+# This program shows off common string-related operations in Python3
+# By: Nick from CoffeeBeforeArch
+
+# Make a string variable
+s = "Test string"
+
+# Print the first 4 characters using a slice
+print(s[0:4:1])
+
+# Print the remainder of a string
+print(s[5:])
+
+# Split the spring into an array of strings
+s = s.split(" ")
+print(s)
+
+# Combine the stings back together
+s = " ".join(s)
+print(s)
